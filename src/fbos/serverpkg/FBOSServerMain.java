3cb889caf2e004613840ace5dd690f57ed136411
Daniel Curran <daniel.curran@RACKSPACE.com>
2012-04-21 20:58:39 -0500

    first commit

---
+++ src/fbos/serverpkg/FBOSServerMain.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fbos.serverpkg;
+
+/**
+ *
+ * @author User
+ */
+public class FBOSServerMain {
+    
+}
